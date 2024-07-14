91283a663e1d854412816bc99b1e3b3eeb64bcdd
Greezaaa <greezaaa@gmail.com>
2024-07-14 12:15:51 +0200

    chore: Update Helmet translation

--- src/layout/main/Menu/Menu.component.tsx
+++ src/layout/main/Menu/Menu.component.tsx
@@ -1,32 +1,50 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import PizzaComponent from "@components/menu/Pizza";
-import BruschettasComponent from "@components/menu/Bruschettas";
-import PastaComponent from "@components/menu/Pasta";
-import pastaImage from "@assets/images/5x5/pasta_pesto.webp";
-const MenuComponent = () => {
-  const url = window.location.href;
-  const title = "Menu | Pasta e Pizza - Da Kiko";
-  const description = "Your page description here.";
-  const imageUrl = `${url}/${pastaImage}`;
+import { useTranslation } from "react-i18next";
+
+import MenuComponent from "@layout/main/Menu/Menu.component";
+import HeaderComponent from "@layout/Header/Header.component";
+import TopBannerComponent from "@layout/Header/TopBanner.component";
+import FooterComponent from "@layout/Footer/Footer.component";
+
+import BackgroundComponent from "@components/background/BackgroundComponent";
+import CookieBanner from "@components/cookie-banner/CookieBanner";
+import HomeComponent from "../Home/HomeComponent";
+
+const Main: React.FC = () => {
+  const { t } = useTranslation("global");
 
   return (
     <>
       <Helmet>
-        <title>{title}</title>
-        <meta property="og:url" content={url} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={imageUrl} />
-        <meta name="description" content={description} />
+        <title>{t("global.seo.openGraph.title")}</title>
+        <meta
+          name="description"
+          content={t("global.seo.metaTags.description")}
+        />
+        <meta name="keywords" content={t("global.seo.metaTags.keywords")} />
+        <meta property="og:title" content={t("global.seo.openGraph.title")} />
+        <meta
+          property="og:description"
+          content={t("global.seo.openGraph.description")}
+        />
+        <meta property="og:image" content={t("global.seo.openGraph.image")} />
+        <meta property="og:url" content={t("global.seo.openGraph.url")} />
+        <meta property="og:type" content={t("global.seo.openGraph.type")} />
       </Helmet>
-      <div className="content">
-        <PizzaComponent />
-        <PastaComponent />
-        <BruschettasComponent />
-      </div>
+      <BackgroundComponent />
+      <TopBannerComponent />
+      <HeaderComponent />
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="/menu" element={<MenuComponent />} />
+        <Route path="*" element={<HomeComponent />} />
+      </Routes>
+      <CookieBanner />
+      <FooterComponent />
     </>
   );
 };
 
-export default MenuComponent;
+export default Main;
